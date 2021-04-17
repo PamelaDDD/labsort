5dd8bd368016e2bdc5d46fb1276a7bca0cf51d15
pamela <duwenjing.cn@gmail.com>
2021-04-17 22:23:23 +0800

    update sort: print the last num of data after reading

--- sort.c
+++ sort.c
@@ -12,9 +12,7 @@ int float_cmp(const void *a, const void *b){
 }
 
 int main(int argc, char* argv[]){
-    /**
     int data_size;
-    int i,j;
     double cost;
     if(argc < 3) {
         printf("Usage:%s data_faile, data_size\n",argv[0]);
@@ -31,8 +29,9 @@ int main(int argc, char* argv[]){
     }
     data = (float *)malloc(sizeof(float)*BLOCK*data_size);
     printf("malloc space for data success!\n");
-    read(handle,data,BLOCK*data_size*sizeof(float));
+    read(handle,data,sizeof(float)*BLOCK*data_size);
     printf("read success!\n");
+    printf("the last num of data %f\n",data[data_size*BLOCK-1]);
     clock_t start_time = clock();
     qsort(data, data_size*BLOCK, sizeof(float),float_cmp);
     clock_t end_time = clock();
@@ -41,12 +40,9 @@ int main(int argc, char* argv[]){
     printf("the firt num is :%f, the last num is :%f, the cost is %f \n",data[0],data[data_size*BLOCK-1],cost);
     close(handle);
     free(data);
-    **/
+/*
     float *test;
     test = (float *)malloc(sizeof(float)*5);
-//    for(int i = 0; i < 5; i++){
-//        test[i] = (float)i+0.8;
-//    }
     test[0] = 0.0;
     test[1] = 0.9;
     test[2] = 0.87;
@@ -57,4 +53,5 @@ int main(int argc, char* argv[]){
         printf("number %d : %f \t",i,test[i]);
     }
     printf("\n");
+    */
 }
